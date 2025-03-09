4913a7124bc80cf897de882aea602f79c32a7e23
DariaAntonovna <DariaAntonovna>
2025-03-09 20:01:04 +0300

    Сохраняю изменения перед сменой репозитория

---
+++ hw-4.js
@@ -0,0 +1,58 @@
+//Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2
+let b = ('Года выпуска первого iPhone - June 29, 2007');
+alert(b);
+
+//Задание 3
+let c = ('Создатель языка JavaScript - Brendan Eich');
+alert(c);
+
+//Задание 4
+let d = 10
+let i = 2
+alert(d + i)
+alert(d - i)
+alert(d * i)
+alert(d / i)
+
+//Задание 5
+let result = i ** 5
+alert(result)
+
+//Задание 6 
+let f = 9
+let j = 2
+let remainder = 9 % 2
+alert(remainder)
+
+//Задание 7 
+let num = 1
+num += 5
+num -= 3
+num *= 7
+num /= 3
+num++
+num--
+alert(num)
+
+//Задание 8
+let age = prompt('Сколько Вам лет?')
+alert(age)
+
+//Задание 9
+let use = {
+    name: "Daria",
+    age: 26,
+    isAdmin: true
+}
+console.log(use)
+
+//Задание 10
+let username = prompt('Как тебя зовут?');
+alert(`Привет, ${username}!`);
+
