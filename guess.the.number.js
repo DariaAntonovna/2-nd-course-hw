49594979f916c906c044737e8889dc4beac1590b
DariaAntonovna <DariaAntonovna>
2025-03-25 15:54:45 +0300

    Домашнее задание 2.5 Функции

---
+++ guess.the.number.js
@@ -0,0 +1,39 @@
+function guessNumberGame() {
+    const randomNumber = Math.floor(Math.random() * 100 + 1);
+    let attempts = 0;
+
+    while (true) {
+        const userAttempts = prompt('Угадай число от 1 до 100');
+
+        if (userAttempts === null) {
+            alert('Конец игры. Загаданное число было:' + randomNumber);
+            return;
+        }
+
+        const num = Number(userAttempts);
+
+        if (isNaN(num)) {
+            alert('Пожалуйста, введите число!');
+            continue;
+
+        }
+
+        attempts++;
+
+        if (num === randomNumber) {
+            alert(`Поздравляю, ты угадал число ${randomNumber} за ${attempts} попыток!`);
+            return;
+        }
+
+        else if (num < randomNumber) {
+
+            alert('Мое число больше. Попробуй еще раз!')
+
+        } else {
+            alert('Мое число меньше. Попробуй еще раз!')
+
+        }
+    }
+
+}
+
