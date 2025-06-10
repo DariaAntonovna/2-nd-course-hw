0a56fa8a539280a85d8b612345c5c094747f8b39
DariaAntonovna <DariaAntonovna>
2025-06-10 16:19:05 +0300

    Проверка курсовой работы 10.06.25

--- flipthetext.js
+++ flipthetext.js
@@ -2,16 +2,6 @@ function flipTheText(text) {
 
     const textUser = prompt('Введите текст, который нужно перевернуть:');
 
-    if (textUser === null) {
-        document.getElementById('result').textContent = 'Вы отвенили ввод!';
-        return;
-    }
-
-    if (textUser.trim() === '') {
-        document.getElementById('result').textContent = 'Вы не ввели текст!';
-        return;
-    }
-
     const reversedText = textUser.split('').reverse().join('');
 
     alert(`Перевёрнутый текст:\n${reversedText}`);
